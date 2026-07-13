95389afc0e97f898faa2a5b0716a0a428d0dbcd0
Naveen Kumar <pkyy8215@gmail.com>
2026-07-13 15:42:01 +0530

    Improve responsive navbar and search UI

--- app.js
+++ app.js
@@ -52,8 +52,8 @@ const store=MongoStore.create({
     touchAfter:24*3600,
 });
 
-store.on("error",()=>{
-    console.log("Error in Mongo Session Store",error);
+store.on("error",(err)=>{
+    console.log("Error in Mongo Session Store",err);
 })
 
 const sessionOptions={
